6801e7bf91139a7a8a5832871e43d7e195d6fb60
Kristiana Kirk <93414377+kristiana11@users.noreply.github.com>
2024-02-26 15:52:50 -0700

    Revert "Day 1 and Day 2 Project"
    This reverts commit e1e6bb4de53a1b91ebacbc8c2f60b4184d93a4f1.

--- BandNameGenerator.py
+++
@@ -1,9 +0,0 @@
-# print out welcome message
-print("Welcome to the Band Name Generator.\n")
-
-# get user input for city and pet name
-city = input("What's the name of the city you gre up in?\n")
-pet_name = input("\nWhat's your pet's name?\n")
-
-# display band name
-print("\nYour band name could be " + city + " " + pet_name)
